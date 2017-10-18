da176c31abca4979b83a6b14c58d784bdcb654cb
supercupcu <hoanhtuan81pk@gmail.com>
2017-10-18 08:35:29 +0700

    fix throw error

--- bittrex/test/simple_tool.py
+++ bittrex/test/simple_tool.py
@@ -1,4 +1,3 @@
-
 import json
 from bittrex.bittrex import Bittrex
 from decimal import *
@@ -8,34 +7,41 @@ class TradingBittrex:
     ask = 0
     volum = 0
     bid = 0
+
     def setUp(self):
         with open("secrets.json") as secrets_file:
             self.secrets = json.load(secrets_file)
             secrets_file.close()
         self.bittrex = Bittrex(self.secrets['key'], self.secrets['secret'])
 
-    def buyCoin(self,market,bit):
+    def buyCoin(self, market, bit):
         ticket = self.bittrex.get_ticker(market)
         self.ask = ticket["result"]["Ask"]
-        self.volum = bit/self.ask
-        # actual = self.bittrex.buy_limit(market,self.volum,self.ask)
+        self.volum = bit / self.ask
+        actual = self.bittrex.buy_limit(market, self.volum, self.ask)
+        return actual
 
-    def sellCoin(self,market,callback):
+    def sellCoin(self, market, callback):
         ticket = self.bittrex.get_ticker(market)
         self.bid = ticket["result"]["Bid"]
-        priceUp= self.ask*1.05
-        priceDown = self.ask*0.95
-        count =0
-        while((priceDown<=self.bid)&(self.bid<=priceUp)):
-            count += 1
-            ticket = self.bittrex.get_ticker(market)
-            self.bid = round(Decimal(ticket["result"]["Bid"]),8)
-            callback("time "+str(count)+": bid = "+str(self.bid))
-            print("\nwaiting... bid="+str(self.bid)+" time:"+str(count))
-        print("\nsucess")
-        # self.bittrex.sell_limit(market,self.volum,self.bid)
+        priceUp = self.ask * 1.1
+        priceDown = self.ask * 0.95
+        count = 0
+        while ((priceDown <= self.bid) & (self.bid <= priceUp)):
+            try:
+                count += 1
+                ticket = self.bittrex.get_ticker(market)
+                self.bid = round(Decimal(ticket["result"]["Bid"]), 8)
+                callback("time " + str(count) + ": bid = " + str(self.bid))
+                print("\nwaiting... bid=" + str(self.bid) + " time:" + str(count))
+            except Exception:
+                print("Error connect")
 
-    def run(self,market,bit,callback):
+        print("\nsucess")
+        self.bittrex.sell_limit(market, self.volum, self.bid)
+
+    def run(self, market, bit, callback):
         self.setUp()
-        volum = self.buyCoin(market,bit)
-        ask = self.sellCoin(market,callback)
+        volum = self.buyCoin(market, bit)
+        if volum['success']:
+            ask = self.sellCoin(market, callback)
